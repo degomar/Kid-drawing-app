bf882cf695d67fd6caa4870d6f0233c3f1fdab74
Eric <eric.degomar@gmail.com>
2023-02-17 14:59:15 -0300

    incluido databinding

--- app/src/main/java/com/example/kidsdrawinhgapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawinhgapp/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.kidsdrawinhgapp
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kidsdrawinhgapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
